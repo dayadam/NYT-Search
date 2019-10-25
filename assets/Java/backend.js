d1aa887f86715bae6e00249740ca6416246e6933
Triston Tetley <tetleylax@aol.com>
2019-10-25 14:28:04 -0400

    added event listeners to .js

--- assets/Java/backend.js
+++ assets/Java/backend.js
@@ -1,9 +1,37 @@
-const nytSearch = "https://api.nytimes.com/svc/search/v2/articlesearch.json&api-key=xiiaiW9gywlqQiH0sWvyQIklMTWxCBVP?q="+searchTerm;
-const searchTerm = "Trump";
+$(document).ready(function () {
 
-$.ajax({
-    url: nytSearch,
-    method: "Get"
-}).then(function(response){
-    console.log(response);
+    console.log('running')
+
+    $("#submit-btn").click(function(event){
+        event.preventDefault();
+        
+    });
+    $("#clear-btn").click(function(event){
+        event.preventDefault();
+        
+    });
+    $("#example-search").click(function(event){
+        event.preventDefault();
+    });
+    $("#exampleFormControlSelect").click(function(event){
+        event.preventDefault();
+    });
+    $("#exampleStartYear").click(function(event){
+        event.preventDefault();
+    });
+    $("exampleEndYear").click(function(event){
+        event.preventDefault();
+    })
+
+
+
+    const nytSearch = "https://api.nytimes.com/svc/search/v2/articlesearch.json&api-key=xiiaiW9gywlqQiH0sWvyQIklMTWxCBVP?q=" + searchTerm;
+    const searchTerm = "Trump";
+
+    $.ajax({
+        url: nytSearch,
+        method: "Get"
+    }).then(function (response) {
+        console.log(response);
+    });
 });
