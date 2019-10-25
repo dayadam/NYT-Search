53e4d2445864bc3095d3e72c48ef240207dcdcb1
Bradley Pagano <bpagano5243@gmail.com>
2019-10-25 14:27:10 -0400

    adding working JS

--- assets/Java/backend.js
+++ assets/Java/backend.js
@@ -1,9 +1,28 @@
-const nytSearch = "https://api.nytimes.com/svc/search/v2/articlesearch.json&api-key=xiiaiW9gywlqQiH0sWvyQIklMTWxCBVP?q="+searchTerm;
-const searchTerm = "Trump";
+
+const headline = "Trump";
+const beginYear = "";
+const endYear = "";
+//const userInput = $(".form-control");
+let records = 5;
+const searchTerm = {
+    q: headline,
+}
+if(endYear){
+    searchTerm.end_date = endYear;
+}
+if(beginYear){
+    searchTerm.begin_date = beginYear;
+}
+const paramString = $.param(searchTerm);
+const nytSearch = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=xiiaiW9gywlqQiH0sWvyQIklMTWxCBVP&q="+searchTerm;
 
 $.ajax({
     url: nytSearch,
     method: "Get"
 }).then(function(response){
-    console.log(response);
+    console.log(response.response.docs);
+    for (i=0;i<records;i++){
+        console.log(response.docs[i].headline.main);
+    }
+
 });
